599fef422cf871d0b5ca81173b9fed3956405424
UMin <liu198@sheridancollege.ca>
2023-05-23 16:13:44 -0400

    I’m Done

--- src/exercise1/CardTrick.java
+++ src/exercise1/CardTrick.java
@@ -54,6 +54,8 @@ public class CardTrick {
      * A simple method to print out personal information. Follow the instructions to 
      * replace this information with your own.
      * @author Paul Bonenfant Jan 2022
+     * @author UMin May 23, 2023
+     * I’m done!
      */
     private static void printInfo() {
     
